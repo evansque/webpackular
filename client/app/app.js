d518d3538b0d4c919e2190281f0cf6c575f278f9
AymanJitan <ayman.jitan@gmail.com>
2016-11-28 16:35:22 -0500

    code clean up

--- client/app/app.js
+++ client/app/app.js
@@ -1,36 +1,35 @@
-import "bootstrap-sass/assets/stylesheets/_bootstrap.scss";
+import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss';
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import config from './config/app.config';
 import Common from './common/common';
-import ngTranslate from "angular-translate";
-import "angular-translate-loader-static-files";
+import ngTranslate from 'angular-translate';
+import 'angular-translate-loader-static-files';
 import AppComponent from './app.component';
 import Components from './components/components';
-import "bootstrap-sass/assets/javascripts/bootstrap.min.js";
-
+import 'bootstrap-sass/assets/javascripts/bootstrap.min.js';
 
 
 angular.module('app', [
-    'app.config',
-	uiRouter,
-	Common.name,
-	Components.name,
-    ngTranslate
+  'app.config',
+  uiRouter,
+  Common.name,
+  Components.name,
+  ngTranslate
 ])
-.config(($urlRouterProvider,$locationProvider,$translateProvider,ENV)=>{
+  .config(($urlRouterProvider, $locationProvider, $translateProvider, ENV) => {
     $translateProvider.useStaticFilesLoader({
-        prefix: 'app/i18n/',
-        suffix: '.json'
+      prefix: 'app/i18n/',
+      suffix: '.json'
     });
 
     $translateProvider.useSanitizeValueStrategy('escaped');
     $translateProvider.preferredLanguage('en');
 
     $locationProvider.html5Mode({
-        enabled: true,
-        requireBase: false
+      enabled: true,
+      requireBase: false
     });
     $urlRouterProvider.otherwise('/');
-})
-.directive('app', AppComponent);
+  })
+  .directive('app', AppComponent);
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,38 @@
 path = require('path');
 module.exports = {
-    devtool: 'sourcemap',
-    output: {
-        filename: 'bundle.js'
-    },
-    module: {
+  devtool: 'sourcemap',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: [/app\/lib/, /node_modules/],
+        loader: 'babel'
+      },
+      {
+        test: /\.html$/,
+        loader: 'raw'
+      },
+      {
+        test: /\.scss$/,
         loaders: [
-            { test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loader: 'babel' },
-            { test: /\.html$/, loader: 'raw' },
-            { test: /\.scss$/, loader: "style!css!sass?outputStyle=expanded&includePaths[]=" + path.resolve(__dirname, "./node_modules/compass-mixins/lib") + '!sass-resources'},
-            { test: /\.css$/,  loader: 'style!css' },
-            { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
+          'style','' +
+          'css',
+          'sass?outputStyle=expanded&includePaths[]=' + path.resolve(__dirname, './node_modules/compass-mixins/lib'),
+          'sass-resources'
         ]
-    },
-    sassResources: path.resolve(__dirname, "./client/app/resources/stylesheets/base.scss")
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css'
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
+      }
+    ]
+  },
+  sassResources: path.resolve(__dirname, './client/app/resources/stylesheets/base.scss')
 };
